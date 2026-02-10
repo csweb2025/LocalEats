b65fcc2d15df2a409e4b657e4cbe264300b38867
Sergiu <sergiuceica@yahoo.com>
2026-02-10 18:09:55 +0200

    Added index entry point

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
 const express = require("express");
 
+const dashboardRouter = require("../routes/dashboard");
+
 const app = express();
 
 app.use(express.json());
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
+app.use('/dashboard', dashboardRouter);
 
 app.get("/", (req, res) => {
-  res.send("Server started successfully.");
+  res.render("index");
 });
+
+
+// Middleware for error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    success: false,
+    message: 'Eroare internă a serverului',
+    error: process.env.NODE_ENV === 'development' ? err.message : 'Eroare internă'
+  });
+});
+
+// Middleware for handling 404 Not Found
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: 'Rută negăsită'
+  });
+});
+
 
 module.exports = app;
